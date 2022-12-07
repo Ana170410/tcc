75e7bf89092ac52613296efbbc1a9f0781ef2df4
AnaJulia1017 <115761184+AnaJulia1017@users.noreply.github.com>
2022-12-07 01:17:56 -0300

    Add files via upload

---
+++ entrarcomlogin.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="ferramenta-de-mao.png">
+    <title>Como posso ajudar?</title>
+    <style>
+      body{
+          font-family: Arial, Helvetica, sans-serif;
+          background-image: linear-gradient(45deg, whitesmoke, white);
+          color: black;
+          justify-content: center;
+          text-align: center;
+      }
+      div{
+          position: relative;
+          padding: 15px;
+          border-radius: 15px;
+          color: #fff;
+          justify-content: center;
+      }
+      strong{
+          color:#f68c68;
+      }
+      label,h2,p{
+          color:black;
+          text-align: center;
+      }
+      a{
+            text-decoration: none;
+            color:white;
+            border: none;
+            background-color: #f68c68;
+            border-radius: 15px;
+            padding: 10px;
+            text-align: left;
+        }
+        a:hover{
+            background-color: #f59a7c;
+        }
+  </style>
+</head>
+<body>
+<br>
+  <div class="form-group">
+  <h2><strong>Entrar com login na plataforma</strong></h2><br>
+  <p><a href="http://localhost/tela-login/login.php">Login</a></label></p><br>
+  <p>Para entrar com o login é necessário ter feito o cadastro.</p><br>
+  <p><a href="http://localhost/formulario/formulario-login.php">Cadastro</a></p><br>
+  <p>Caso já tenha feito ignore a mensagem.</p><br><br>
+  </div>
+  <a href="http://localhost/home-page/home.php">Voltar para o ínicio</a><br>
+</body>
+</html>
